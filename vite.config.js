fe091c57f7ffc4d50c58f85ef6a2f4fafe3ce07d
AJAY BANDIWADDAR <ajaybandiwaddar1@gmail.com>
2026-08-12 13:51:01 +0530

    Set base path for Vite configuration

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,7 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig({
+  base: '/queue-doctor/',
   plugins: [react()],
   build: {
     outDir: 'dist'
